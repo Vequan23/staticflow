a9f790787ec2590ee0d540d1d112d80c48acfd6d
Vequan Clark <vequanclark@vequans-mbp.lan>
2021-11-26 23:56:51 -0600

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,16 +10,18 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve(__dirname, "public/js"),
   },
-  rules: [
-    {
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env"],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
       },
-    },
-  ],
+    ],
+  },
 };
